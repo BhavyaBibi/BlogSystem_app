14950c5abe4dde526eb423368f42e8602517573d
Bhavya Bibi <bhavyabibi007@gmail.com>
2021-03-25 22:31:57 +0400

    code is worked

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -5,29 +5,29 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+    2. Add a URL to urlpatterns:  url('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  url('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    1. Import the include() function: from django.urls import include, url
+    2. Add a URL to urlpatterns:  url('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from blog.views import index,about, post, contact, search, view_all
+from blog.views import index, about, post, contact, search, view_all
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name = 'home'),
     path('about/', about, name = 'about'),
-    path('post/', post, name = 'post'),
+    path('post/<int:id>/<slug:slug>', post, name = 'post'),
     path('contact/', contact, name = 'contact'),
     path('search/', search, name = 'search'),
-    path('view_all/', view_all, name = 'view_all')
+    path('view_all/<str:query>', view_all, name = 'view_all'),
 ]
 
 if settings.DEBUG:
